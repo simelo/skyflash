f527c537035d1a97e48ad6175ff4ba196f261ae9
Pavel Milanes (CO7WT) <dev0032@simelo.tech>
2019-07-06 03:53:54 -0400

    Refs #64, add a plain make installation

--- travis/install.sh
+++ travis/install.sh
@@ -7,7 +7,7 @@ python3 --version ; pip3 --version
 if [ $1 = 'linux' ]; then
     # linux
     sudo apt update -q
-    sudo apt install -y python3 python3-all python3-pip python3-setuptools python3-wheel
+    sudo apt install -y python3 python3-all python3-pip python3-setuptools python3-wheel make
     pip3 install -U pip setuptools PyInstaller
     pip3 install -r requirements.txt
 fi
