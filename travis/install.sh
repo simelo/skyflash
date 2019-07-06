8a0a8652e3d10769c6268bece37652435f92e87d
Pavel Milanes (CO7WT) <dev0032@simelo.tech>
2019-07-06 03:11:15 -0400

    Refs #64, improve the installation of python requirements via a default file

--- travis/install.sh
+++ travis/install.sh
@@ -6,24 +6,28 @@ python3 --version ; pip3 --version ; sw_vers
 
 if [ $1 = 'linux' ]; then
     # linux
-    sudo apt-get update -q
-    sudo apt install -y python3 python3-all python3-pip python3-setuptools python3-wheel make
-    sudo pip3 install setuptools pyqt5 PyInstaller
+    pip3 install -U pip PyInstaller
+    pip3 install -u requirements.txt
 fi
 
 
 if [ $1 = 'osx' ]; then
     # macos
-    pip install --upgrade pip
-    pip3 install setuptools pyqt5 PyInstaller
+    pip3 install -U pip PyInstaller
+    pip3 install -r requirements.txt
 fi
 
 
 if [ $1 = 'windows' ]; then
     # windows
     sudo apt-get update -q
-    sudo apt install -y p7zip-full
+    sudo apt install -y python3 python3-all python3-pip python3-setuptools python3-wheel p7zip-full make
+    pip3 install -U setuptools
+    pip3 install -r requirements.txt
 
     # pull the docker for windows
     docker pull cdrx/pyinstaller-windows
 fi
+
+# Explicit showdown of relevan software
+python3 --version ; pip3 --version ; sw_vers
